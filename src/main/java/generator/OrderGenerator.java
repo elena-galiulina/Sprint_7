b51434fcf1b11ee16912040ed851aca1f28a8d3a
elena-galiulina <ev.galiulina@yandex.ru>
2025-09-23 22:16:06 +0500

    откат до java11

--- src/main/java/generator/OrderGenerator.java
+++ src/main/java/generator/OrderGenerator.java
@@ -9,13 +9,16 @@ import static utils.Utils.randomString;
 public class OrderGenerator {
     public static Order randomOrder() {
         Random random = new Random();
+        int minValue = 1;
+        int maxValue = 7;
+        int rentTime = minValue + random.nextInt(maxValue - minValue + 1);
         return new Order()
                 .setFirstName(randomString())
                 .setLastName(randomString())
                 .setAddress(randomString())
                 .setMetroStation(randomString())
                 .setPhone(randomString())
-                .setRentTime(random.nextInt(1,7))
+                .setRentTime(rentTime)
                 .setComment(randomString());
     }
 }
